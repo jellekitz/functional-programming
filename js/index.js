373afa0970a71c06a4eb95e21e1a149e358e9c89
Jellekitz <jellekitz@gmail.com>
2021-10-26 20:32:46 +0200

    added lowercase method to object values

--- js/index.js
+++ js/index.js
@@ -4,8 +4,12 @@ let dataset = fetch("../data.json")
     // log de data naar de console
     // console.log(data);
 
+    // function deCap(str) {
+    //   let lowercase = str.toLowerCase();
+    // }
+
     // voor elk object laten we alleen de value zien van de onderstaande key
     data.forEach((obj) => {
-      console.log(obj["Wat is je favoriete soort huisdier?"]);
+      console.log(obj["Wat is je favoriete soort huisdier?"].toLowerCase());
     });
   });
