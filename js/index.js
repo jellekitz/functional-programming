aff031182e82f1beb5bd85283e0dda5ccafb88f2
Jellekitz <jellekitz@gmail.com>
2021-10-28 09:45:07 +0200

    removed comment

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 let dataset = fetch("../data.json")
   .then((result) => result.json())
   .then((data) => {
-    // console.log(data);
     // voor elk object laten we alleen de value zien van de onderstaande key
     data.forEach((obj) => {
       const favPet = obj["Wat is je favoriete soort huisdier?"];
