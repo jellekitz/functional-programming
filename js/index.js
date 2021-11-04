e023f433ca59c1c2ef46fe1c89687b80ed7cebcc
Jellekitz <jellekitz@gmail.com>
2021-11-04 11:42:45 +0100

    add comments to code

--- js/index.js
+++ js/index.js
@@ -4,8 +4,8 @@ import {
   noSpace,
   addSpace,
   translateLabel,
-} from "./sanitize.js";
+} from "./sanitize.js"; // importeer alle schoonmaak functies vanuit sanitize.js
 
-import { displayArticles } from "./render.js";
+import { displayArticles } from "./render.js"; // importeer de displayArticles function uit render.js
 
-import { dataset } from "./fetch.js";
+import { dataset } from "./fetch.js"; // importeer de fetch uit fetch.js
